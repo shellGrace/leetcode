12feb05272e4c708321ea2aed257c6e6f4e5023b
shellGrace <lijuan@lijuandeMBP.lan>
2022-03-15 00:36:24 +0800

    add removeNthFromEnd

---
+++ 算法/简单/删除链表的倒数第N个节点.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let len = 0
+    // 计算链表长度
+    let h = head
+    let pre = head
+    while(h !== null) {
+        len++
+        h = h.next
+    }
+    // 删除的是头节点
+    if(len - n === 0) return head.next
+    // 得到被删除节点的前一个
+    for(let i = 0; i < len - n - 1; i++) {
+        pre = pre.next
+    }
+    // 前一个指向节点后一个 删除
+    pre.next = pre.next.next
+    return head
+};
