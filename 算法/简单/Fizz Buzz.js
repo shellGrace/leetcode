0ab85b2532746bf2513c1283f896583a808f73a5
shellGrace <lijuan@lijuandeMBP.lan>
2022-09-05 00:33:43 +0800

    add fizzBuzz

--- 算法/简单/Fizz Buzz.js
+++ 算法/简单/Fizz Buzz.js
@@ -3,5 +3,25 @@
  * @return {string[]}
  */
  var fizzBuzz = function(n) {
-
+    let answer = new Array(n)
+    for(let i = 0; i < n; i++) {
+        let index = i + 1
+        if(index % 3 === 0 && index % 5 === 0) {
+          answer[i] = "FizzBuzz"
+        } else if(index % 3 === 0) {
+            answer[i] = "Fizz"
+        } else if(index % 5 === 0) {
+            answer[i] = "Buzz"
+        } else {
+            answer[i] = String(index)
+        }
+    }
+    return answer
 };
+
+// answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+// answer[i] == "Fizz" 如果 i 是 3 的倍数。
+// answer[i] == "Buzz" 如果 i 是 5 的倍数。
+// answer[i] == i （以字符串形式）如果上述条件全不满足。
+
+console.log('fizzBuzz', fizzBuzz(3));
