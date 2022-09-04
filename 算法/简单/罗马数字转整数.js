f37981ba39069c98f4990d6608317eb8fc81dd4c
shellGrace <lijuan@lijuandeMBP.lan>
2022-09-04 17:02:45 +0800

    feat: add romanToInt

--- 算法/简单/罗马数字转整数.js
+++ 算法/简单/罗马数字转整数.js
@@ -14,19 +14,15 @@
     }
     let amount = 0
     let i = 0
-    while(i < s.length) {
-        let item = s[i]
-        let next = s[i+1]
-        let temp = romanMap[item]
-        let ivx = item === 'I' && (next === 'V' || next === 'X')
-        let xlc = item === 'X' && (next === 'L' || next === 'C')
-        let cdm = item === 'C' && (next === 'D' || next === 'M')
-        if(ivx || xlc || cdm) {
-            amount += romanMap[next] - temp
-            i += 2
-            continue
+    let len = s.length
+    while(i < len) {
+        let cur = romanMap[s[i]]
+        let next = romanMap[s[i+1]]
+        if(i < (len - 1) && cur < next) {
+            amount -= cur
+        } else {
+            amount += cur
         }
-        amount += temp
         i++
     }
     return amount
@@ -35,6 +31,8 @@
 // I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
 // X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
 // C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+
+// 通常情况下，罗马数字中小的数字在大的数字的右边。
 
 let s = 'LVIII' // 58
 console.log('romanToInt', romanToInt(s));
