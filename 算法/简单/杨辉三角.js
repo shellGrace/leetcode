beb450747b36a24ce4e96d0bc7dd6dfd01fa023e
shellGrace <lijuan@lijuandeMBP.lan>
2022-09-18 02:19:46 +0800

    add generate

--- 算法/简单/杨辉三角.js
+++ 算法/简单/杨辉三角.js
@@ -6,11 +6,11 @@
     if(numRows === 0) return 0
     let res = [[1]]
     let preRow = [1]
-    let row = []
     for(let i = 1; i < numRows; i++) {
         // 处理row数组
-        preRow.splice(0, 0, 0)
-        preRow.splice(i+1, 0, 0)
+        preRow.unshift(0)
+        preRow.push(0)
+        let row = []
         let j = 0
         while(j < preRow.length - 1) {
             let e = preRow[j] + preRow[j+1]
@@ -20,7 +20,6 @@
         res.push(row) 
         // 初始化
         preRow = [...row]
-        row = []
     }
     return res
 };
