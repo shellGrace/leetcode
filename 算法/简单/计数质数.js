6b9250b2f0e74958947cbd2fa1fa7ff2ccb4c4ad
shellGrace <lijuan@lijuandeMBP.lan>
2022-09-18 03:04:32 +0800

    add countPrimes

--- 算法/简单/计数质数.js
+++ 算法/简单/计数质数.js
@@ -2,6 +2,22 @@
  * @param {number} n
  * @return {number}
  */
- var countPrimes = function(n) {
-
+var countPrimes = function (n) {
+  const isPrime = function (num) {
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
 };
+
+console.log("countPrimes", countPrimes(10));
