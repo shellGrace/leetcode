fe302ce3f8e0c45c15350a860ea74c023ccf9d76
shellGrace <lijuan@lijuandeMBP.lan>
2022-09-18 23:17:51 +0800

    add countPrimes

--- 算法/简单/计数质数.js
+++ 算法/简单/计数质数.js
@@ -4,6 +4,8 @@
  */
 var countPrimes = function (n) {
   const isPrime = function (num) {
+    // 如果它不是质数，一定有两个数相乘等于它。这两个因数一定有一个因数小于等于根号
+    // i * i 为了避免测试用例超时做时间复杂度优化
     for (let i = 2; i * i <= num; i++) {
       if (num % i === 0) {
         return false;
