d0e3d8c8ded1b2921c2c71028764b11d2ec9501e
shellGrace <2420082764@qq.com>
2023-09-25 23:46:21 +0800

    update

--- hot-100/77-组合.js
+++ hot-100/77-组合.js
@@ -3,20 +3,22 @@
  * @param {number} k
  * @return {number[][]}
  */
- var combine = function(n, k) {
-    let result = []
-    const combineHelp = (start, cur) => {
-        if(cur.length === k) {
-            // 满足条件, 递归终止
-            result.push([...cur])
-            return
-        }
-        for(let i = start; i <= n; i++) {
-            cur.push(i)
-            combineHelp(i + 1, cur)
-            cur.pop()
-        }
-     }
-     combineHelp(1, [])
-     return result
- };
+var combine = function (n, k) {
+  let result = [];
+  const combineHelp = (start, cur) => {
+    if (cur.length === k) {
+      // 满足条件, 递归终止
+      result.push([...cur]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      cur.push(i);
+      combineHelp(i + 1, cur);
+      cur.pop();
+    }
+  };
+  combineHelp(1, []);
+  return result;
+};
+
+console.log(combine(4, 2));
